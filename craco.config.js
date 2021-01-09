098cb924abe7f65c6f2c72e98907ef1ec66dc6e7
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-01-09 18:30:03 +0100

    Add craco configuration for @emotion/babel-preset-css-prop
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,20 @@
 const CracoAlias = require('craco-alias');
 
+// To avoid using the pragma for EmotionJS `css` prop:
+// https://github.com/emotion-js/emotion/issues/1123#issuecomment-455767886
+const emotionPresetOptions = {};
+
+const emotionBabelPreset = require("@emotion/babel-preset-css-prop").default(
+  undefined,
+  emotionPresetOptions
+);
+
 module.exports = {
+  babel: {
+    plugins: [
+      ...emotionBabelPreset.plugins
+    ]
+  },
   plugins: [
     {
       plugin: CracoAlias,
